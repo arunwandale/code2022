32c6287f2f70b93e5a55072a85722ce58dad9103
arunwandale <84247687+arunwandale@users.noreply.github.com>
2022-02-17 00:58:49 +0530

    cool tech added

--- sample.java
+++ sample.java
@@ -2,5 +2,6 @@ class sample{
   public static void main(String args[]){
     System.out.println("Hello Git");
     System.out.println("Hello Git Hub");
+    System.out.println("Hello Git Hub is cool techonogy to manage code");
   }
 }
